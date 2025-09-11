9ad7446be03272cbcc26cd0fb7b2750bfff5b3fd
K01t0N <125848629+K01t0N@users.noreply.github.com>
2025-09-11 18:31:42 +0000

    Created backend

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,15 +1,16 @@
 module.exports = {
   root: true,
-  env: { browser: true, node: true, es6: true },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
